be42b67022cbfbb568975ec6df5abaaecc2af846
Manish Bastola <manishbastola47@gmail.com>
2025-02-26 19:45:33 -0600

    my first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,45 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import todologo from './assets/todo-image.png';
 import './app.css';
 
 function App() {
-  const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
-  const addTask = () => {
-    if (task) {
-      setTasks([...tasks, task]);
-      setTask('');
-    }
-  };
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
 
-  const deleteTask = (index) => {
-    let newTask = [...tasks]; 
-    delete newTask[index];    
-    let updatedTask = [] 
-    for (let i = 0; i<newTask.length; i++){
-      if (newTask[i] !== undefined) {
-        updatedTask.push(newTask[i])
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos?");
+        const data = await response.json();
+        setTasks(data);
+      } 
+      
+      catch (err) {
+        console.error("getting error while fetching", err);
       }
-    }
-    setTasks(updatedTask)
-  };
+    };
+    
+    fetchTasks();
+  }, []);
 
   return (
     <div className="app-container">
       <div>
-        <a href="https://ca.indeed.com/career-advice/career-development/daily-to-do-list" target="_blank">
+        <a
+          href="https://ca.indeed.com/career-advice/career-development/daily-to-do-list"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={todologo} className="logo" alt="To-Do logo" />
         </a>
       </div>
 
-      <h1>Make Your To-Do-List :)</h1>
-
-      <div>
-        <input
-          className="input-task"
-          type="text"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-          placeholder="Enter a task"
-        />
-        <button onClick={addTask} className="insert-btn">
-          Insert
-        </button>
-      </div>
+      <h1>Make Your To-Do List :)</h1>
 
       <ul className="task-list">
-        {tasks.map((task, index) => (
-          <li key={index} className="task-item">
-            <input type="checkbox" />
-            <span>{task}</span>
-            <button onClick={() => deleteTask(index)} className="delete-btn">
-              Delete
-            </button>
+        {tasks.map((task) => (
+          <li key={task.id} className="task-item">
+            <span>{task.title}</span>
           </li>
         ))}
       </ul>
@@ -64,4 +48,3 @@ function App() {
 }
 
 export default App;
-
